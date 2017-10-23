3440ec834e6ca412193df41b635066dc29d378d7
Dhaval Jardosh <dhavaljardosh@gmail.com>
2017-10-23 15:26:08 -0500

    Skeleton Ready

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  // name = 'welcome to Angular';
+  test: string;
+  name: string;
+  age: number;
+  email: string;
+  address: Address
+  hobbies: string[];
 
-  constructor() { }
+  constructor() {
+    console.log('constructor ran...')
+   }
 
   ngOnInit() {
+    console.log('ngOnInit ran...');
+    this.test = "Test";
+    this.name = "Angular";
+    this.age = 30;
+    this.address = {
+      street: '50 Main Street',
+      city:'Boston',
+      state:'MA'
+    }
+    this.hobbies = ['Write code','Watch movies','Listen to music']
   }
 
+  onClick(){
+    this.name = "Dhaval Jardosh";
+    this.hobbies.push("New Hobby")
+  }
+
+  addHobby(hobby){
+    console.log(hobby);
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+
+  deleteHobby(hobby){
+    for(let i=0; i<this.hobbies.length;i++){
+      if(this.hobbies[i]=== hobby){
+        this.hobbies.splice(i,1);
+      }
+    }
+  }
+
+  refreshString(){
+    console.log("Refresh Button");
+    return false;
+  }
+
+}
+
+interface Address{
+  street: string,
+  city: string,
+  state: string
 }
