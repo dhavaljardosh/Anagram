2ad2cc143e52dc712c9f0dfa697ca09e312a341f
Dhaval Jardosh <dhavaljardosh@gmail.com>
2017-10-24 10:49:17 -0500

    Added Comments

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,19 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   // name = 'welcome to Angular';
-  test: any[] = [1,4];
+  test: any = "Input";
 
   constructor() {
     console.log('constructor ran...')
    }
 
   ngOnInit() {
+    //https://stackoverflow.com/questions/39927452/recursively-print-all-permutations-of-a-string-javascript
+    //Will be trying to implement this logic using 2 way binding
+
+    /*
+    Take the input from the user from
+    */
 
   }
 
   refreshString(){
 
     console.log(this.test);
+    this.test = "";
+    /* Here will be the logic to select next element in the Array of so, it will
+    as expected
+    And if "Refresh" click is equal to number of elements in array,
+    It will again start from the first.
+
+    Even making this Random is possible as well.
+    */
     return false;
   }
 
